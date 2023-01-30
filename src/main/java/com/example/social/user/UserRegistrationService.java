d4c191fbd6d248bc6bf43cb18e3e88507a380066
jay <jay.wekeep@gmail.com>
2023-01-30 09:11:08 +0900

    DB 설정 및 Entity 생성, 기본 인증 서비스 구현

---
+++ src/main/java/com/example/social/user/UserRegistrationService.java
@@ -0,0 +1,21 @@
+package com.example.social.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRegistrationService {
+    
+    private final UserFindService userFindService;
+    private final UserRepository userRepository;
+
+    public void requestRegistration (final String name, final String email) {
+
+        final boolean exists = userFindService.existsByEmail(email);
+
+        if (!exists) {
+            userRepository.save(new User(name, email));
+        }
+    }
+}
